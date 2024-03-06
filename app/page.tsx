4d25ee66b9f8fb715843cd93087d0418238cadcd
learnuidev <learnuidev@gmail.com>
2024-03-05 23:29:51 -0500

    added constants

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,18 @@
 
 import { Navbar } from "@/components/nav-bar";
 import { Button } from "@/components/ui/button";
+import { homePage } from "./home-page.constants";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <h1 className="font-bold text-center mt-56 mb-4 text-5xl">
-        ts engineering
+      <h1 className="font-bold text-center mt-56 mb-4 text-5xl md:text-7xl">
+        {homePage.header.title}
       </h1>
       <p className="mx-16 text-center text-2xl font-extralight">
-        {/* next gen professional social network */}
-        next gen software engineering social network
+        {homePage.description.title}
       </p>
-
       <Button variant="outline" className="rounded-full my-8 mx-auto block">
         Get Started
       </Button>
