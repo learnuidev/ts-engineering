d19c654830b7204ac874ace500486df39ce59c78
learnuidev <learnuidev@gmail.com>
2024-03-05 23:29:05 -0500

    refactor home page

---
+++ app/home-page.constants.ts
@@ -0,0 +1,12 @@
+export const homePage = {
+  header: {
+    title: "ts engineering",
+  },
+  description: {
+    title: "networking app from the future",
+  },
+
+  getStartedButton: {
+    title: "Get Started",
+  },
+};
---
+++ app/search/page.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Icons } from "@/components/icons";
+import { NavButton, Navbar } from "@/components/nav-bar";
+import { Input } from "@/components/ui/input";
+import clsx from "clsx";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <main className="mt-56 mx-4 md:mx-32">
+        <Input
+          autoFocus
+          placeholder="Ask anything"
+          className={clsx(
+            "md:h-16 border-none rounded-none focus-visible:ring-transparent block mx-auto font-exralight text-center focus-visible:ring-0",
+            "placeholder:text-gray-200 placeholder:font-light placeholder:text-2xl md:placeholder:text-4xl",
+            "text-2xl md:text-4xl"
+          )}
+        />
+
+        <hr className="" />
+
+        <div className="my-8 space-x-8 flex justify-center items-center">
+          <NavButton>
+            <Icons.magnifyingGlass />
+          </NavButton>
+          <NavButton>
+            <Icons.gear />
+          </NavButton>
+        </div>
+      </main>
+    </div>
+  );
+}
