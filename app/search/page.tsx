ee0f03c1e7c50010dff7efd6ff63323e07aea2cf
learnuidev <learnuidev@gmail.com>
2024-03-07 00:33:39 -0500

    changed search term font's weight to extralight

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         className={clsx(
           "md:h-16 border-none rounded-none focus-visible:ring-transparent block mx-auto font-exralight text-center focus-visible:ring-0",
           "placeholder:text-gray-200 placeholder:font-extralight placeholder:text-2xl md:placeholder:text-4xl",
-          "text-2xl md:text-4xl"
+          "text-2xl md:text-4xl font-extralight"
         )}
       />
 
